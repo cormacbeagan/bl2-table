1e4d1c7b28422bf1e02537b89f988e82d432d14b
cormacbeagan <cormacbeagan@gmail.com>
2021-09-11 11:29:24 +0200

    ...

--- api/index.js
+++ api/index.js
@@ -1,15 +1,15 @@
 import express from "express";
-import morgan from "morgan";
-import cors from "cors";
-import helmet from "helmet";
+// import morgan from "morgan";
+// import cors from "cors";
+// import helmet from "helmet";
 import { scraper } from "./functions/scraper.js";
 
 const app = express();
-const PORT = process.env.PORT || 8888;
+// const PORT = process.env.PORT || 8888;
 
-app.use(helmet());
-app.use(cors());
-app.use(morgan("dev"));
+// app.use(helmet());
+// app.use(cors());
+// app.use(morgan("dev"));
 // app.use(express.static(__dirname));
 
 app.get("/", async (req, res) => {
@@ -17,7 +17,7 @@ app.get("/", async (req, res) => {
     const htmlString = await scraper();
     res.status(200).header("Content-Type", "text/html").send(htmlString);
   } catch (err) {
-    console.log(err);
+    // console.log(err);
     res.status(400).send();
   }
 });
@@ -26,6 +26,8 @@ app.get("/", async (req, res) => {
 //   res.sendFile("../public/style.css");
 // });
 
-app.listen(PORT, async () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+// app.listen(PORT, async () => {
+//   console.log(`Server listening on port ${PORT}`);
+// });
+
+module.exports = app;
