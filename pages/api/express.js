e766e7c9dec9ba9ec11c7656cf63a7dd59a5fe7e
cormacbeagan <cormacbeagan@gmail.com>
2021-09-17 09:10:34 +0200

    remove route

--- pages/api/express.js
+++
@@ -1,9 +0,0 @@
-import { scraper } from "../../functions/scraper.js";
-export default function handler(req, res) {
-  try {
-    const htmlString = await scraper();
-    res.status(200).header("Content-Type", "text/html").send(htmlString);
-  } catch (err) {
-    res.status(400).send();
-  }
-}
